1bcd97d570871a09a00fd3679ddf3b68567a1cac
chaurasiaaditya7a22-netizen <chaurasiaaditya7a22@gmail.com>
2025-11-29 11:47:55 +0530

    Create MainActivity.java

---
+++ android/app/src/main/python/android/app/src/main/android/app/src/main/java/com/adityaspendflow/MainActivity.java
@@ -0,0 +1,11 @@
+package com.adityaspendflow;
+
+import android.os.Bundle;
+import com.chaquo.python.android.PythonActivity;
+
+public class MainActivity extends PythonActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
